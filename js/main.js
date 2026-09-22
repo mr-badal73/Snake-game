dabacd6cffdfd7d01cecc401d7ec5ff37bea0629
Badal Choudhary <badal.24scse1010366@galgotiasuniversity.ac.in>
2026-09-22 15:06:24 +0530

    snake

---
+++ js/main.js
@@ -0,0 +1,57 @@
+import { Game } from './game.js';
+
+const board = document.getElementById('game-board');
+
+let game;
+let interval;
+
+const createBoard = () => {
+    board.innerHTML = "";
+
+    for (let y = 0; y < game.rows; y++) {
+        for (let x = 0; x < game.columns; x++) {
+
+            const cell = document.createElement('div');
+
+            cell.classList.add('cell');
+
+            cell.dataset.x = x;
+            cell.dataset.y = y;
+
+            board.appendChild(cell);
+        }
+    }
+};
+
+const Render = () => {
+
+    const cells = board.children;
+
+    game.snake
+        .getBody()
+        .forEach((segment, index) => {
+
+            const cell = board.querySelector(
+                `[data-x="${segment.x}"][data-y="${segment.y}"]`
+            );
+
+            if (!cell) return;
+
+            cell.classList.add("snake");
+
+            if (index === 0) {
+                cell.classList.add("head");
+            }
+        });
+};
+
+const startGame = () => {
+
+    game = new Game();
+
+    createBoard();
+
+    Render();
+};
+
+startGame();
